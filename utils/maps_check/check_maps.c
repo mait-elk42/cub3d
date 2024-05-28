1371abd627b11c37a4e2c71f0396841a75348110
Mohamed Ait Elkadi <mait-elk@e3r3p16.1337.ma>
2024-05-28 18:18:32 +0100

    fixed

--- utils/maps_check/check_maps.c
+++ utils/maps_check/check_maps.c
@@ -6,7 +6,7 @@
 /*   By: mait-elk <mait-elk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 16:40:46 by mait-elk          #+#    #+#             */
-/*   Updated: 2024/05/28 17:17:26 by mait-elk         ###   ########.fr       */
+/*   Updated: 2024/05/28 18:17:54 by mait-elk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,12 @@ void	maps_iteri(char **maps, t_vector2 pos, bool *found_plr)
 	}
 	if (safe_strchr("0NSEW", maps[pos.y][pos.x]) != NULL)
 	{
+		if ((pos.y == 0 && safe_strchr(maps[pos.y], '0')) || (pos.x == 0 && maps[pos.y][pos.x] == '0'))
+			eput_error("error maps in the rounds", maps[pos.y], 1);
 		if (pos.y > 0 && (safe_strlen(maps[pos.y -1]) < (size_t)pos.x || maps[pos.y -1][pos.x] == ' '))
 			eput_error("error maps in the TOP", maps[pos.y], 1);
 		if (safe_strlen(maps[pos.y +1]) < (size_t)pos.x || maps[pos.y +1][pos.x] == ' ')
 			eput_error("error maps in the DOWN", maps[pos.y], 1);
-		
 		if (pos.x > 0 && maps[pos.y][pos.x -1] == ' ')
 			eput_error("error maps in the LEFT", maps[pos.y], 1);
 		if (maps[pos.y][pos.x +1] == ' ')
@@ -52,7 +53,7 @@ void	check_maps(void)
 		pos.x = 0;
 		while (maps[pos.y][pos.x])
 		{
-			maps_iteri(maps, pos, &found_plr);			
+			maps_iteri(maps, pos, &found_plr);
 			pos.x++;
 		}
 		pos.y++;
